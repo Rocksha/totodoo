a1c2f9fd70f92493a86f252c472d043f8dedce58
rocksha <sraksha91@gmail.com>
2016-10-05 08:02:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$("ul li").draggable();
+$("#list ul").sortable({
+    items:"li:not('.day, .addItem')",
+    connectWith:"ul",
+    dropOnEmpty: true,
+    placeholder:"emptySpace"
+});
+
+
+$(".addItem input").keydown(function(e){
+    if (e.keyCode == 13){
+        var item = $(this).val();
+        $(this).parent().parent().append('<li>'+item+'</li>');
+        $(this).val("");
+    }
+});
+$('#deleter').droppable({
+    drop: function(event, ui){
+        ui.draggable.remove();
+    }
+});
